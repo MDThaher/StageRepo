f899cd15fe54a707ca0c3818544d809b8596a838
MDThaher <thahergithub@gmail.com>
2024-03-19 16:26:34 +0530

    Revert "test"
    This reverts commit 119200470879edadd455694daf2dd31803948694.

--- TestPack/test.java
+++ TestPack/test.java
@@ -3,7 +3,6 @@ package TestPack;
 class test {
 
     public static void main(String[] args) {
-       System.out.println("hello test staged");
        System.out.println("hello test staged"); 
     }
 }
